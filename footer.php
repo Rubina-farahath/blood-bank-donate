adb53716d913d6889148f62e93205d42e299e3b1
Rubina-farahath <rubinafarahath@gmail.com>
2025-06-11 19:24:58 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,26 @@
+<html>Add commentMore actions
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 75px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" style="background-color:#000000;height:75px;width:100%;color:white" >
+  <b>COPYRIGHT 2020<br>
+  Blood Bank & Donation
+  <div id="footer" >
+  <b><center>COPYRIGHT © 2020<br>
+  Blood Bank & Donation Management
+  <br>
+  ALL RIGHTS RESERVED.
+  
