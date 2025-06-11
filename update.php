96bc4bdd6a1f088d58345a65e0dc3c7ef88d8227
Rubina-farahath <rubinafarahath@gmail.com>
2025-06-11 19:27:47 +0530

    Add files via upload

---
+++ update.php
@@ -0,0 +1,67 @@
+ <?php Add commentMore actions
+  $active ='need';
+  include('head.php') ?>
+<?php
+$active ='donate';
+ include('head.php') ?>
+
+  <div id="page-container" style="margin-top:50px; position: relative;min-height: 84vh;">
+    <div class="container">
+    <div id="content-wrap" style="padding-bottom:50px;">
+
+  <div class="row">
+      <div class="col-lg-6">
+          <h1 class="mt-4 mb-3">Need Blood</h1>
+
+        </div>
+  </div>
+  <form name="needblood" action="" method="post">
+  <div class="row">
+  <div class="col-lg-4 mb-4">
+  <div class="font-italic">Blood Group<span style="color:red">*</span></div>
+  <div><select name="blood" class="form-control" required>
+    <option value=""selected disabled>Select</option>
+    <?php
+      include 'conn.php';
+      $sql= "select * from blood";
+      $result=mysqli_query($conn,$sql) or die("query unsuccessful.");
+    while($row=mysqli_fetch_assoc($result)){
+     ?>
+     <option value=" <?php echo $row['blood_id'] ?>"> <?php echo $row['blood_group'] ?> </option>
+    <?php } ?>
+<div id="page-container" style="margin-top:50px; position: relative;min-height: 84vh;">
+  <div class="container">
+  <div id="content-wrap" style="padding-bottom:50px;">
+<div class="row">
+    <div class="col-lg-6">
+        <h1 class="mt-4 mb-3">Donate Blood </h1>
+      </div>
+</div>
+<form name="donor" action="savedata.php" method="post">
+<div class="row">
+<div class="col-lg-4 mb-4">
+<div class="font-italic">Full Name<span style="color:red">*</span></div>
+<div><input type="text" name="fullname" class="form-control" required></div>
+</div>
+<div class="col-lg-4 mb-4">
+<div class="font-italic">Mobile Number<span style="color:red">*</span></div>
+<div><input type="text" name="mobileno" class="form-control" required></div>
+</div>
+<div class="col-lg-4 mb-4">
+<div class="font-italic">Email Id</div>
+<div><input type="email" name="emailid" class="form-control"></div>
+</div>
+</div>
+<div class="row">
+<div class="col-lg-4 mb-4">
+<div class="font-italic">Age<span style="color:red">*</span></div>
+<div><input type="text" name="age" class="form-control" required></div>
+</div>
+<div class="col-lg-4 mb-4">
+<div class="font-italic">Gender<span style="color:red">*</span></div>
+<div><select name="gender" class="form-control" required>
+<option value="">Select</option>
+<option value="Male">Male</option>
+<option value="Female">Female</option>
+</select>
+</div>
