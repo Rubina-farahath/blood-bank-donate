6673bdb95ed5661ce98cec71197a49a81d912f78
Rubina-farahath <rubinafarahath@gmail.com>
2025-06-11 19:24:07 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php include 'head.php'; ?>Add commentMore actions
+<?php $active ='contact';
+include 'head.php'; ?>
+<?php
+if(isset($_POST["send"]))
+  $name=$_POST['fullname'];
+------------------------------------
+u
